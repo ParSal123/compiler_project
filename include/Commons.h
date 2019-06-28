beec00a2e101415144c8ded804486d2a16edcbb2
Parsa Salavati <parsa.salavati@gmail.com>
2019-06-28 22:55:41 +0430

    Simplified grammar for future use

--- include/Commons.h
+++ include/Commons.h
@@ -9,9 +9,9 @@
 
 using namespace std;
 
-const string PROGRAM_ADDRESS = "../res/input.txt";
-const string ERRORS_ADDRESS = "../errors.txt";
-const string PARSE_TREE_ADDRESS = "../parseTree.txt";
+const string PROGRAM_ADDRESS = "../in/input.txt";
+const string ERRORS_ADDRESS = "../out/errors.txt";
+const string PARSE_TREE_ADDRESS = "../out/parseTree.txt";
 const string GRAMMAR_ADDRESS = "../res/grammar.txt";
 const string FIRST_FOLLOW_ADDRESS = "../res/firstfollow.txt";
 
@@ -20,4 +20,5 @@ constexpr int ERROR_TOKEN_ID = 0;
 constexpr int EPSILON_TOKEN_ID = 1;
 constexpr int NUM_TOKEN_ID = 2;
 constexpr int ID_TOKEN_ID = 3;
+
 #endif //COMPILER_PROJECT_COMMONS_H
